c15f1ff808747bb135b7fe2ac9baf95b4252db36
Raul de Arriba <raul@raul.raul>
2017-12-04 00:17:20 +0100

    test end

--- PartidosFCVB.py
+++ PartidosFCVB.py
@@ -6,18 +6,17 @@ TOKEN = '495586156:AAGkSobt7tgtYaS6ug2-BdIfUgCgntnwvzY' # Ponemos nuestro Token 
 
 tb = telebot.TeleBot(TOKEN) # Combinamos la declaración del Token con la función de la API
 tb_update = Updater(tb.TOKEN)
-def start(bot, update, pass_chat_data=True):
-	update.message.chat_id
-	tb.send_message(chat_id=update.message.chat_id, 'Hola mundo!') # Ejemplo tb.send_message('109556849', 'Hola mundo!')
 
-start_handler = commandHandler('start', start)
 
-dispatcher = tb.dispatcher
+@tb.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Howdy, how are you doing?")
 
-dispatcher.add_handler(start_handler)
+@tb.message_handler(func=lambda message: True)
+def echo_all(message):
+	bot.reply_to(message, message.text)
 
-tb.start_polling()
-tb.idle()
+bot.polling()
 
 while True:
 	pass
