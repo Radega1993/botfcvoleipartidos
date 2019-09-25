3fc108b1418a7072cbafc878f3b29219c8bd6450
Raul-logicalis <rde@externos.itnow.es>
2019-09-25 08:35:54 +0200

    add menu structure

--- PartidosFCVB.py
+++ PartidosFCVB.py
@@ -4,6 +4,7 @@
 import telebot
 from telebot import types
 #imports de las webs
+from resources.getInfo import get_info
 from partidosinfantilA import *
 from partidosinfantilB import *
 from partidoscadeteA import *
@@ -15,7 +16,7 @@ from partidossenior import *
 # token from the librari
 TOKEN = 'token'
 
-tb = telebot.TeleBot(TOKEN) 
+tb = telebot.TeleBot(TOKEN)
 
 @tb.message_handler(commands=['start'])
 def send_welcome(message):
@@ -72,6 +73,37 @@ def comando_jornadasenior(mensaje):
 	chat_id = mensaje.chat.id
 	jornada = jornadaSenior()
 	tb.send_message(chat_id, jornada)
+
+
+#https://github.com/eternnoir/pyTelegramBotAPI#general-use-of-the-api
+
+# Using the ReplyKeyboardMarkup class
+# It's constructor can take the following optional arguments:
+# - resize_keyboard: True/False (default False)
+# - one_time_keyboard: True/False (default False)
+# - selective: True/False (default False)
+# - row_width: integer (default 3)
+# row_width is used in combination with the add() function.
+# It defines how many buttons are fit on each row before continuing on the next row.
+markup = types.ReplyKeyboardMarkup(row_width=2)
+itembtn1 = types.KeyboardButton('a')
+itembtn2 = types.KeyboardButton('v')
+itembtn3 = types.KeyboardButton('d')
+markup.add(itembtn1, itembtn2, itembtn3)
+tb.send_message(chat_id, "Choose one letter:", reply_markup=markup)
+
+# or add KeyboardButton one row at a time:
+markup = types.ReplyKeyboardMarkup()
+itembtnesc = types.KeyboardButton('Escola')
+itembtninf = types.KeyboardButton('infantil')
+itembtncad = types.KeyboardButton('Cadet')
+itembtnjuv = types.KeyboardButton('juvenil')
+itembtnsen = types.KeyboardButton('senior')
+itembtnmas = types.KeyboardButton('Master')
+markup.row(itembtnesc, itembtninf)
+markup.row(itembtncad, itembtnjuv)
+markup.row(itembtnsen, itembtnmas)
+tb.send_message(chat_id, "Escull un equip:", reply_markup=markup)
 
 
 
---
+++ resources/getInfo.py
@@ -0,0 +1,32 @@
+from bs4 import BeautifulSoup
+import urllib2
+
+def get_info(equipo):
+    equipo = quote_page
+    #quote_page = 'http://competicio.fcvoleibol.cat/competiciones.asp?v=18&torneo=2359'
+
+	page = urllib2.urlopen(quote_page)
+	pageHtml = page.read()
+	page.close()
+	soup = BeautifulSoup(pageHtml, "lxml")
+
+	name_box = soup.find('div', attrs={'id':'nombre_competicion'})
+	jornada_box = soup.find('div', attrs={'id':'jornada_numero'})
+	tabla_box = soup.find('div', attrs={'class':'resultados'})
+
+	name = name_box.text.strip()
+	jornada = jornada_box.text.strip()
+	tabla = tabla_box.text.strip()
+
+	msg = ""
+	msg += name + '\n'
+
+	msg += jornada + '\n'
+
+
+	lista = tabla.split("  ")[8:]
+
+	for element in lista:
+		msg += element + '\n'
+
+	return msg
